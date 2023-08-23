58f108ccc1d6b17ec1a2ac445342555bb561dd33
mateusb12 <mateus_b09@hotmail.com>
2023-08-23 11:24:25 -0300

    Initial commit

---
+++ references/path_reference.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+
+
+def getMainFolderPath() -> Path:
+    return Path(os.path.dirname(os.path.realpath(__file__))).parent
+
+
+def getTestPlanCsvFolderPath() -> Path:
+    return getMainFolderPath() / 'requestTests/testPlans'
+
+
+def __main():
+    testPlan = getTestPlanCsvFolderPath()
+    print(testPlan)
+    return
+
+
+if __name__ == '__main__':
+    __main()
