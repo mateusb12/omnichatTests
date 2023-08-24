55556b2ca2cf69258d108b720ef29315e66f5327
mateusb12 <mateus_b09@hotmail.com>
2023-08-24 14:05:09 -0300

    "Refactor automated tests and update test plans
    Made several changes to 'automatedConversationTest.py' in order to make tests more robust and readable by incorporating a less strict comparison method which normalizes string comparisons. This method eliminates discrepancies due to variations in white space. Tests are now run using pytest, improving the automation of our test workflow.
    In addition, the git repository now ignores pytest cache files, which should not be version controlled.
    Lastly, made minor changes to the test plan 'dialogflowPlan.csv', presumably to align with changes in the application's behavior."

--- requestTests/automatedTests/automatedConversationTest.py
+++ requestTests/automatedTests/automatedConversationTest.py
@@ -1,5 +1,7 @@
-import pandas as pd
+import re
 
+import pandas as pd
+import pytest
 from requestTests.automatedTests.testPlanLoader import getDialogflowMessagesPlan
 from requestTests.httpCalls import sendTwilioRequest, convertResponseToUtf8
 
@@ -8,25 +10,48 @@ def colorize(text: str, color_code: str):
     return f"\033[{color_code}m{text}\033[0m"
 
 
-def runTestPlan(testPlan: pd.DataFrame):
-    inputMessageList, expectedMessageList = testPlan["inputMessages"], testPlan["expectedMessages"]
+def normalize_string(s: str) -> str:
+    """Convert sequences of whitespace to a single space and trim the string."""
+    return re.sub(r'\s+', ' ', s).strip()
+
+
+def less_strict_comparison(str1: str, str2: str) -> bool:
+    normalized_str1 = normalize_string(str1)
+    normalized_str2 = normalize_string(str2)
+    return normalized_str1 == normalized_str2
+
+
+def test_run_plan():
+    plan = getDialogflowMessagesPlan()
+    inputMessageList, expectedMessageList = plan["inputMessages"], plan["expectedMessages"]
     combinedMessages = list(zip(inputMessageList, expectedMessageList))
     for actual, expected in combinedMessages:
-        print(colorize(f"User: {actual}", "1;36"))
+        print(colorize(f"→               [User]: {actual}", "1;36"))
         rawResponse = sendTwilioRequest(body=actual)
         textResponse = convertResponseToUtf8(rawResponse)
-        print(colorize(f"Bot: {textResponse}", "1;35"))
+        _actual = textResponse.replace("\n", "")
+        _expected = expected.replace("\n", "")
+        print(colorize(f"Actual:   {_actual}", "0;33"))
+        print(colorize(f"Expected: {_expected}", "0;33"))
         testResult = "Passed" if textResponse == expected else "Failed"
-        result_color = "1;31" if testResult == "Failed" else "1;32"
-        print(colorize(f"Test result: {testResult}", result_color))
+        pytestCheck(textResponse, expected)
+        # result_color = "1;31" if testResult == "Failed" else "1;32"
+        # print(colorize(f"Test result: {testResult}", result_color))
         print("")
     return
 
 
+def pytestCheck(actual, expected):
+    assert less_strict_comparison(actual, expected), f"Expected: '{expected}', Got: '{actual}'"
+
+
+def main_test_runner():
+    test_run_plan()
+
+
 def __main():
-    plan = getDialogflowMessagesPlan()
-    runTestPlan(plan)
+    main_test_runner()
 
 
 if __name__ == "__main__":
-    __main()
+    pytest.main([__file__])
